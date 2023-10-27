7ef5627f2a6892ce8837cc4fb80b0c41a828c8b2
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-10-27 15:26:08 +0530

    Criminal Records Form Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import "./App.scss";
+import { useState } from "react";
 
 const App = () => {
+  const [walletConnected, setWalletConnected] = useState(true);
   return (
     <div className="App">
-      Sdmrf
+      {!walletConnected ? (
+        <div className="Connect">
+          <button>Connect Wallet</button>
+        </div>
+      ) : (
+        <div className="CriminalRecords">
+          <h1>Criminal Records</h1>
+          <form action="">
+            <label>
+              Criminal Id
+              <input type="text" name="criminalId" required />
+            </label>
+            <label>
+              Criminal Personal Info
+              <input type="text" name="personalInfo" required />
+            </label>
+            <label>
+              Criminal Mugshots
+              <input type="file" name="mugshots" title="Upload" className="Upload" required />
+            </label>
+            <button>Submit</button>
+          </form>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
